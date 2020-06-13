44c465673d1a97cd6cf890e7af730162dbd0bf67
Saquib Irtiza <saquibirtiza@users.noreply.github.com>
2020-06-13 18:21:44 -0500

    Server side files

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai = require('clarifai')
+
+const app = new Clarifai.App({
+    apiKey: 'f2156ef67e624f63b99f611aefaac5a3'
+});
+
+const handleApiCall = (req, res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with the API'))
+}
+
+const handleImage = (req, res, db)=> {
+    const {id} = req.body;
+    // let found = false;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries =>{
+        res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
