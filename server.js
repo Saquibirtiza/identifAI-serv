21d0f31cd5866774cfbc2a882d9722b54badf410
Saquib Irtiza <saquibirtiza@users.noreply.github.com>
2020-06-13 20:13:03 -0500

    changed database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-tapered-55264',
         user: 'postgres',
         password: 'UTDallas@1973',
         database: 'finalproject'
